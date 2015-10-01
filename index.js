ffd52e80f13ec0ccee1d499f75a325f02a6f0bae
Tom Brennan <tommybrennan@gmail.com>
2015-09-30 20:21:33 -0400

    small changes to the index js file

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
-    // YOUR CODE HERE!
+    $.ajax({
+      url: "https://api.myjson.com/bins/2sadq?pretty=1",
+      dataType: "json",
+      success: function(response) {
+        $.each(response.apartments, function(i, apartment) {
+            var listing = "<a href='#' class='list-group-item'><h4 class='list-group-item-heading'>" + apartment.description + " / " + apartment.bedrooms +  " BR / " + apartment.price +"</h4><p class='list-group-item-text'>" + apartment.neighborhood + "</p></a>"
+            $(".apartments").append(listing);
+        });
+      },
+      error: function(error) {
+        console.log(error);
+      }
+    });
+
+    $(".filter").click(function() {
+      var city = $(this).attr("id");
+    });
 });
 
 
