68480ac7b0261e91c1396033b0338abb2049ffd0
Tom Brennan <tommybrennan@gmail.com>
2015-10-03 13:54:08 -0400

    added google maps functionality to the page

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ $(document).ready(function() {
       dataType: "json",
       success: function(response) {
         $.each(response.apartments, function(i, apartment) {
-            var listing = "<a href='#' class='list-group-item'><h4 class='list-group-item-heading'>" + apartment.description + " / " + apartment.bedrooms +  " BR / " + apartment.price +"</h4><p class='list-group-item-text'>" + apartment.neighborhood + "</p></a>"
+          var apartmentClass = apartment.city.toLowerCase().replace(" ", "-");
+            var listing = "<a href='#' id=" + apartment.id + " class='list-group-item " + apartmentClass + " listings'><h4 class='list-group-item-heading'>" + apartment.description + " / " + apartment.bedrooms +  " BR / " + apartment.price +"</h4><p class='list-group-item-text'>" + apartment.neighborhood + "</p></a>"
             $(".apartments").append(listing);
         });
       },
@@ -14,8 +15,38 @@ $(document).ready(function() {
     });
 
     $(".filter").click(function() {
+      $(".filter").removeClass("active");
+      $(this).addClass("active");
+
       var city = $(this).attr("id");
+      $(".listings").show();
+
+      if (city !== "all") {
+        $(".listings").not("." + city).css("display", "none");
+      }
     });
+
+        $(document).on("click", ".listings", function() {
+          var id = $(this).attr("id");
+          $.ajax({
+              url: "https://api.myjson.com/bins/2sadq?pretty=1",
+              dataType: "json",
+              success: function(response) {
+                  var selectedApartment = $.grep(response.apartments, function(apartment) {
+                    return apartment.id == id;
+                  })
+                  var address = selectedApartment[0].address;
+
+                  window.open("http://maps.google.com/?q=" + address);
+              },
+              error: function(error) {
+                console.log(error);
+              }
+        });
+
+
+    });
+
 });
 
 
